551246840d002c51286411cfb7db55fa041db7bc
adnansyed101 <sirgeant10@gmail.com>
2023-02-12 13:06:26 +0600

    overhaul the html css and JS code

--- app.js
+++ app.js
@@ -1,93 +1,145 @@
-const bookTable = document.querySelector(".book-info"),
-  bookForm = document.getElementById("book-form"),
-  titleInput = document.getElementById("title"),
-  authorInput = document.getElementById("author"),
-  statusInput = document.getElementById("status");
-
+// Book Class
 class Book {
-  constructor(title, author, status) {
+  constructor(title, author, isbn) {
     this.title = title;
     this.author = author;
-    this.status = status;
+    this.isbn = isbn;
+  }
+}
+
+// UI class
+class UI {
+  // Display books
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function (book) {
+      UI.addBookToList(book);
+    });
   }
 
-  static addBookToDOM() {
-    let output = ``;
-    for (let i = 0; i < myLibrary.length; i++) {
-      output += `<tr>
-    <td>${myLibrary[i].title}</td>
-    <td>${myLibrary[i].author}</td>
-    <td><button class='status' data-number='${i}'>${myLibrary[i].status}</button></td>
-    <td><button class='button-primary delete' data-number='${i}'>Delete</button></td>
-    </tr>`;
-    }
-    return output;
+  // Add a book
+  static addBookToList(book) {
+    const list = document.getElementById('book-list');
+
+    // Create row
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href='#' class='btn btn-danger btn-sm delete'>X</a></td>`;
+
+    list.appendChild(row);
   }
 
-  static addBookToMyLibrary() {
-    myLibrary.push(
-      new Book(titleInput.value, authorInput.value, statusInput.value)
-    );
-    bookTable.innerHTML = Book.addBookToDOM();
+  // show Alert
+  static showAlert(message, className) {
+    // create div
+    const div = document.createElement('div');
+    //div class
+    div.className = `alert alert-${className}`;
+    // dic text
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+
+    container.insertBefore(div, form);
+
+    setTimeout(function () {
+      document.querySelector('.alert').remove();
+    }, 2000);
   }
 
-  static deleteBook(e) {
-    const arrayIndex = e.target.dataset.number;
-    if (e.target.classList.contains("delete")) {
-      e.target.parentNode.parentNode.remove();
-      const index = myLibrary.indexOf(myLibrary[arrayIndex]);
-      if (index > -1) {
-        myLibrary.splice(index, 1);
+  // Clear field
+  static clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+  }
+
+  // Delete Book
+  static deleteBook(target) {
+    if (target.classList.contains('delete')) {
+      if (confirm('Are you sure?')) {
+        target.parentElement.parentElement.remove();
       }
-    }
-    e.preventDefault();
-  }
-
-  static changeStatus(e) {
-    const arrayIndex = e.target.dataset.number;
-
-    if (e.target.classList.contains("status")) {
-      if (e.target.innerText === "NOT READ") {
-        e.target.innerText = "Read";
-        myLibrary[arrayIndex].status = "Read";
-      } else {
-        e.target.innerText = "Not Read";
-        myLibrary[arrayIndex].status = "Not Read";
-      }
-    }
-    e.preventDefault();
-  }
-
-  static validityCheck(state, message) {
-    if (state.validity.valueMissing) {
-      state.setCustomValidity(message);
-      state.reportValidity();
-    } else {
-      state.setCustomValidity("");
     }
   }
 }
 
-let myLibrary = [];
+// Store class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
 
-bookTable.addEventListener("click", Book.changeStatus);
-bookTable.addEventListener("click", Book.deleteBook);
-
-bookForm.addEventListener("submit", (e) => {
-  if (!titleInput.validity.valid) {
-    Book.validityCheck(titleInput, "Please enter a Title name");
-  } else if (!authorInput.validity.valid) {
-    Book.validityCheck(authorInput, "Please enter an Author name");
-  } else {
-    Book.addBookToMyLibrary();
+    return books;
   }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// display book
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// add a book event
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  // GET FROM UI
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value;
+
+  if (title === '' || author === '' || isbn === '') {
+    UI.showAlert('Please Fill In All Fields', 'danger');
+  } else {
+    const book = new Book(title, author, isbn);
+
+    // Add Book To list
+    UI.addBookToList(book);
+
+    // Add book to store
+    Store.addBook(book);
+
+    // show Alert
+    UI.showAlert('Book Added!', 'success');
+
+    // Clear field
+    UI.clearFields();
+  }
+
   e.preventDefault();
 });
 
-titleInput.addEventListener("input", () => {
-  Book.validityCheck(titleInput, "Please enter a Title name");
-});
+// remove book
+document.getElementById('book-list').addEventListener('click', function (e) {
+  UI.deleteBook(e.target);
 
-authorInput.addEventListener("input", () => {
-  Book.validityCheck(authorInput, "Please enter an Author name");
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+  UI.showAlert('Book Deleted', 'success');
 });
